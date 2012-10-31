20249c80dd1ee4e9b65dfa4c24a770f213f331e5
Zhang Wei <zhangwei01_dl@126.com>
2012-10-31 23:12:16 +0800

    LOG:Add functions for home activity

--- src/com/wl/magz/MainActivity.java
+++ src/com/wl/magz/MainActivity.java
@@ -1,15 +1,30 @@
 package com.wl.magz;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Parcelable;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+    private Handler mHandler;
+    private static final int DOWNLODD_COMPLETE = 0;
+    private static final int DOWNLOAD_NET_OFFLINE = 1;
+    private static final int DOWNLAOD_FAILURE = 2;
+    private static final int DOWNLAOD_TIME_OUT = 3;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //TODO ProgressBar displayed unnormal if the activity is not fullScreen WHY?
+
+        mHandler = new ResultHandler();
+
+        createShortcut();
+
+        runFirstDownload();
     }
 
     @Override
@@ -18,5 +33,51 @@ public class MainActivity extends Activity {
         return true;
     }
 	
-	private void test(){}
+	private void createShortcut() {
+	    Intent shortcutIntent = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
+	    shortcutIntent.putExtra("duplicate", false);
+	    shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.shortcut_name));
+	    Parcelable icon = Intent.ShortcutIconResource.fromContext(getApplicationContext(), R.drawable.icon);
+	    shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
+	    shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, new Intent(getApplicationContext() , MainActivity.class));
+	    sendBroadcast(shortcutIntent);
+	}
+
+    private void runFirstDownload() {
+        FirstDownloadThread thread = new FirstDownloadThread();
+        thread.start();
+    }
+
+    private class ResultHandler extends Handler {
+        public void handleMessage(Message msg) {
+            switch(msg.what) {
+            case DOWNLODD_COMPLETE:
+                //TODO
+                break;
+            case DOWNLOAD_NET_OFFLINE:
+                //TODO
+                break;
+            case DOWNLAOD_FAILURE:
+                //TODO
+                break;
+            case DOWNLAOD_TIME_OUT:
+                //TODO
+                break;
+            }
+        }
+    }
+
+    private class FirstDownloadThread extends Thread {
+        public void run() {
+            //TODO
+            //First Download
+
+            //TODO
+            //sendMessage();
+        }
+
+        private void sendMessage(int result) {
+            mHandler.obtainMessage(result).sendToTarget();
+        }
+    }
 }
